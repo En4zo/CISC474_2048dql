474574b6f6dcd51ffb07bc867ecd898ccc0a710e
benroo123 <49972428+benroo123@users.noreply.github.com>
2021-12-06 03:55:47 +0800

    Delete half directory

--- half/gb.py
+++
@@ -1,8 +0,0 @@
-
-DEPTH1 = 128
-DEPTH2 = 128
-BATCH_SIZE = 512
-INPUT_UNIT = 16
-HIDDEN_UNIT = 256
-OUTPUT_UNIT = 4  # four actions could be taken in total
-EXPAND_SIZE = 2 * 4 * DEPTH2 * 2 + 3 * 3 * DEPTH2 * 2 + 4 * 3 * DEPTH1 *2
